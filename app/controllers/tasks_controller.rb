1c83e2c8599e5576544fec7d6a2e61128710969f
Francis Abad <francisongabad@gmail.com>
2019-02-19 17:12:23 +0900

    finished create

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,31 @@
+class TasksController < ApplicationController
+  def index
+    @tasks = Task.all
+  end
+
+  def show
+    @task = Task.find(params[:id])
+  end
+
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(task_params)
+    @task.save
+    # Will raise ActiveModel::ForbiddenAttributesError
+    redirect_to task_path(@task)
+
+  end
+
+  private
+
+  def task_params
+    # *Strong params*: You need to *whitelist* what can be updated by the user
+    # Never trust user data!
+    params.require(:task).permit(:title, :details)
+  end
+
+end
